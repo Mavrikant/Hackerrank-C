10b39b53be02cf34982be3358001032390ba54ca
mavrikant <m.serdar.karaman@ya.ru>
2019-12-11 01:06:21 +0300

    not done yet

--- permutations-of-strings.c
+++ permutations-of-strings.c
@@ -1,21 +1,77 @@
 //https://www.hackerrank.com/challenges/permutations-of-strings/problem
 
+
+// TODO Bitmedi. Devam ediyor.
+/* Sağdan başka. Kendinden sağdakiler arasından en büyüğünü bul
+ * Onun bir solundaki artır
+ * * Next sayısı bul
+ * Sağda kalanları tekrar sırala.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+void str_increment(int n, char **s_orj, int i);
+
 int next_permutation(int n, char **s) {
-    for (int i = n - 1; i >= 0; --i) {
-        for (int j = 0; j < n; ++j) {
-            if (strcmp(s[j], s[i]) < 0) {
-                i--;
-                break;
+    int i;
+    int biggest_on_left = -1;
+
+    for (i = n - 1; i >= 0; i--) {
+        for (int j = n - 1; j < n; j++) {
+            if (strcmp(s[i], s[j]) > 0) {
+                biggest_on_left = i - 1;
+                goto label;
             }
         }
-        printf("%d\n", i);
     }
+    label:
+    if (biggest_on_left == -1) {
+        biggest_on_left = n - 1;
+    }
+    printf("**%s\n", s[biggest_on_left]);
+
+    for (int i = 0; i < n; i++) {
+        printf("%s%c", s[i], i == n - 1 ? '\n' : ' ');
+    }
+    str_increment(n, s, biggest_on_left);
+
+    for (int i = 0; i < n; i++) {
+        printf("%s%c", s[i], i == n - 1 ? '\n' : ' ');
+    }
+    printf("exit\n");
+
+    return 0;
 
 }
+
+void str_increment(int n, char **s_orj, int M) {
+    char **s_sorted = calloc(n, sizeof(char *));
+    char *temp = NULL;
+
+    for (int i = 0; i < n; i++) {
+        s_sorted[i] = calloc(11, sizeof(char));
+        s_sorted[i] = s_orj[i];
+    }
+    for (int j = 0; j < n; ++j) {
+        for (int i = 0; i < n - j; ++i) {
+            if (strcmp(s_sorted[j], s_sorted[i]) < 0) {
+                temp = s_sorted[j];
+                s_sorted[j] = s_sorted[i];
+                s_sorted[i] = temp;
+            }
+        }
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        printf("%s ", s_sorted[i]);
+        free(s_sorted[i]);
+    }
+    free(s_sorted);
+}
+
 
 int main() {
     char **s;
